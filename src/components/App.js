1130ccf5f7e8f97b43a43133af18bdbd5a15e346
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-09 17:54:12 -0400

    added state and handled it

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 import { Form, FormControl, Button } from 'react-bootstrap'
 
 class App extends Component {
+  state = { text: '' }
+
+  handleOnChange = ({ target: { value: text } }) => this.setState({ text })
+
+  handleOnClick = () => console.log(this.state.text)
+
   render() {
     return (
       <div>
         <h2>Note to Self</h2>
-        <Form>
-          <FormControl />
-          <Button>Submit</Button>
+        <Form inline>
+          <FormControl onChange={this.handleOnChange} />
+          {' '}
+          <Button onClick={this.handleOnClick}>Submit</Button>
         </Form>
       </div>
     )
