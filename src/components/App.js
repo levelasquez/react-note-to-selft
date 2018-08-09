180e5a48463143bab62466f17ff7b1db38e467dd
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-09 18:41:56 -0400

    added persistence with cookies and can been deletes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import { Form, FormControl, Button } from 'react-bootstrap'
+import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies'
 import Note from './Note'
+
+const cookie_key = 'NOTES'
 
 class App extends Component {
   state = {
     text: '',
     notes: [],
+  }
+
+  componentDidMount() {
+    const notes = read_cookie(cookie_key)
+
+    this.setState({ notes })
   }
 
   handleOnChange = ({ target: { value: text } }) => this.setState({ text })
@@ -16,6 +25,14 @@ class App extends Component {
     const newNotes = notes.concat(text)
 
     this.setState({ notes: newNotes })
+
+    bake_cookie(cookie_key, newNotes)
+  }
+
+  handleClear = () => {
+    this.setState({ notes: [] })
+
+    delete_cookie(cookie_key)
   }
 
   render() {
@@ -32,6 +49,8 @@ class App extends Component {
         {notes.map((note, index) => (
           <Note key={`${note}-${index}`} note={note} />
         ))}
+        <hr />
+        <Button onClick={this.handleClear}>Clear Notes</Button>
       </div>
     )
   }
